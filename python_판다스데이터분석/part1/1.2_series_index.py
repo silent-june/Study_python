f480311a5e4ff4829eed1e7c7434b18d6ad79118
silent-june <dydwns1401@korea.ac.kr>
2021-04-23 15:19:11 +0900

    판다스 PART1 2회독

--- python_판다스데이터분석/part1/1.2_series_index.py
+++ python_판다스데이터분석/part1/1.2_series_index.py
@@ -10,8 +10,8 @@ print(sr)
 # 결과값을 보면 dtype이 object로 나오는데, 데이터값 중 하나라도 object면 object로 출력되는듯!
 
 # 인덱스 배열은 변수 idx에 저장. 데이터 값 배열은 변수 val에 저장
-idx = sr.index
-val = sr.values
-print(idx)
+idx = sr.index # 시리즈sr 의 인덱스 저장
+val = sr.values # 시리즈sr 의 데이터값 저장
+print(idx) # 인덱스 이름을 가진 것이 아니기 때문에 0부터 5까지 존재한다고만 출력됨
 print('\n')
 print(val)
